55a41ada0939781ded530f420c44c334a6bffd77
ElamineBendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-12-01 15:16:30 -0500

    finished this was cool!

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+
+button.addEventListener('click', changeHex) 
+
+
+function changeHex(){
+    let newHex = []
+    for (let index = 0; index < 6; index++) {
+        const random = hex[Math.floor(Math.random() * 16)]
+        newHex.push(random)
+    } 
+    let newColor = "#" + newHex.join("")
+    console.log(newColor)
+    color.innerHTML = newColor
+    color.style.color = newColor
+}
